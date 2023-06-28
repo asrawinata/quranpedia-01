3e756486290699294748df152a5b7b9f7f41035f
asrawinata <asra.winata@icloud.com>
2023-06-28 15:30:37 +0700

    footer and header not fixed

--- app/Views/page/index.php
+++ app/Views/page/index.php
@@ -1,6 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+  <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Quranpedia</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="<?php echo base_url();?>/assets/css/styles.css" rel="stylesheet" />
     <title>Quranpedia</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -22,6 +35,24 @@
     <link href="<?php echo base_url();?>/assets/css/indexx.css" rel="stylesheet" />
   </head>
 
+  <header class="masthead">
+            <div class="container position-relative">
+                <div class="row justify-content-center">
+                    <div class="col-xl-6">
+                        <div class="text-center text-white">
+                            <!-- Page heading-->
+                            <h3 class="mb-5">Quranpedia is an website built by students of Software Engineering Telkom Unviersity, Quranpedia Explain Quran term with Quran verses and Hadits and Wikipedia </h3>
+                           
+                            <div class="input-group">
+                                <input type="search" class="form-control rounded" placeholder="Surah apa yang ingin anda telusuri?" aria-label="Search" aria-describedby="search-addon" />
+                                <a href="search" class="btn btn-primary" role="button">Cari</a>
+                              </div>
+                              <a href="daftar_surah" class="btn btn-primary" role="button">Daftar Surah</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
   <body class="homepage">
 
     
@@ -82,5 +113,36 @@
       });
     }
   </script>
+  <footer class="footer bg-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 h-100 text-center text-lg-start my-auto">
+                        <ul class="list-inline mb-2">
+                            <li class="list-inline-item"><a href="#!">About</a></li>
+                            <li class="list-inline-item">⋅</li>
+                            <li class="list-inline-item"><a href="#!">Contact</a></li>
+                            <li class="list-inline-item">⋅</li>
+                            <li class="list-inline-item"><a href="#!">Terms of Use</a></li>
+                            <li class="list-inline-item">⋅</li>
+                            <li class="list-inline-item"><a href="#!">Privacy Policy</a></li>
+                        </ul>
+                        <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2023. All Rights Reserved.</p>
+                    </div>
+                    <div class="col-lg-6 h-100 text-center text-lg-end my-auto">
+                        <ul class="list-inline mb-0">
+                            <li class="list-inline-item me-4">
+                                <a href="#!"><i class="bi-facebook fs-3"></i></a>
+                            </li>
+                            <li class="list-inline-item me-4">
+                                <a href="#!"><i class="bi-twitter fs-3"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#!"><i class="bi-instagram fs-3"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
 </html>
 
